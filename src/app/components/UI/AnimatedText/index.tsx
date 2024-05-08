d7dbe571313c95bf4c277e2b14c06e8ff3382971
khilyao <sfwew19@gmail.com>
2024-05-09 00:24:20 +0300

    add disclaimer button

--- src/app/components/UI/AnimatedText/index.tsx
+++ src/app/components/UI/AnimatedText/index.tsx
@@ -1,30 +1,99 @@
-"use client";
+import { Variant, motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
 
-import { useInView } from "framer-motion";
-import { useRef } from "react";
-
-type Props = {
-  text: string;
-  className: string;
+type AnimatedTextProps = {
+  text: string | string[];
+  el?: keyof JSX.IntrinsicElements;
+  className?: string;
+  once?: boolean;
+  repeatDelay?: number;
+  animation?: {
+    hidden: Variant;
+    visible: Variant;
+  };
 };
 
-const AnimatedText = ({ text, className }: Props) => {
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+export const AnimatedText2 = ({
+  text,
+  el: Wrapper = "p",
+  className,
+  once,
+  repeatDelay,
+  animation = defaultAnimations,
+}: AnimatedTextProps) => {
+  const controls = useAnimation();
+  const textArray = Array.isArray(text) ? text : [text];
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: false });
+  const isInView = useInView(ref, { amount: 0.5, once });
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    const show = () => {
+      controls.start("visible");
+      if (repeatDelay) {
+        timeout = setTimeout(async () => {
+          await controls.start("hidden");
+          controls.start("visible");
+        }, repeatDelay);
+      }
+    };
+
+    if (isInView) {
+      show();
+    } else {
+      controls.start("hidden");
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isInView]);
 
   return (
-    <p ref={ref} className={className}>
-      <span
-        style={{
-          transform: isInView ? "none" : "translateX(-400px)",
-          opacity: isInView ? 1 : 0,
-          transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1)",
+    <Wrapper className={className}>
+      <motion.span
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          visible: { transition: { staggerChildren: 0.1 } },
+          hidden: {},
         }}
+        aria-hidden
       >
-        {text}
-      </span>
-    </p>
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={`${line}-${lineIndex}`}>
+            {line.split(" ").map((word, wordIndex) => (
+              <span className="inline-block" key={`${word}-${wordIndex}`}>
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={`${char}-${charIndex}`}
+                    className="inline-block"
+                    variants={animation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </span>
+            ))}
+          </span>
+        ))}
+      </motion.span>
+    </Wrapper>
   );
 };
 
-export default AnimatedText;
+export default AnimatedText2;
